2d39eb533003ab0075b0741211a2249c8329b197
sidd-kishan <aayush.deo1994@gmail.com>
2024-08-14 12:47:24 +0530

    camera frame hook found
    for every 9 calls of onPreviewFrame 1 call to sendCommandToRemote is made on every 333 milli seconds

--- camera_hook.js
+++ camera_hook.js
@@ -1,16 +1,28 @@
 Java.perform(function(){
- 	Java.use("com/lianhezhuli/btnotification/function/device/remotecamera/Preview$TakePictureCallback").sendCaptureData.overload("[B")
-        .implementation = function(paramArrayOfByte) {
-            // first we cast to list
-            //var list = Java.cast(paramArrayOfCallConnection, ArrayList);
-            // iterating the list
-            //for (var i = 0, l = list.size(); i < l; i++) {
-                // casting each element to the object we created earlier
-                //var currentElement = Java.cast(list.get(i), CallConnection);
-                // printing to terminal
-            console.log(paramArrayOfByte.length);
-            //}
-            // executing original c'tor 
-            this.sendCaptureData(paramArrayOfByte);
-        }	
+
+Java.use("com.lianhezhuli.btnotification.function.device.remotecamera.Preview$1").onPreviewFrame.overload('[B', 'android.hardware.Camera').implementation = function(bytearr,camera){
+	console.log("onPreviewFrame called: "+bytearr.length);
+	this.onPreviewFrame(bytearr,camera);
+}
+
+
+const colors = {
+    colorize: (str, cc) => `\x1b${cc}${str}\x1b[0m`,
+    red: str => colors.colorize(str, '[31m'),
+    green: str => colors.colorize(str, '[32m'),
+    yellow: str => colors.colorize(str, '[33m'),
+    blue: str => colors.colorize(str, '[34m'),
+    cyan: str => colors.colorize(str, '[36m'),
+    white: str => colors.colorize(str, '[37m'),
+};
+
+
+var type=Java.use("com.lianhezhuli.btnotification.mtk.btconnection.BluetoothManager");
+
+type.sendCommandToRemote.overload('int','java.lang.String').implementation = function (n,cmd) {
+		console.log(colors.blue("sendCommandToRemote n:"+n+" cmd:"+cmd));
+		this.sendCommandToRemote(n,cmd);
+return;
+}
     });
+	
